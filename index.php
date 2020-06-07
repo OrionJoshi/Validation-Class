4fd9e6462001657f51f710654264766551990c42
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-07 13:17:54 +0545

    Add Html form

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Validation</title>
+</head>
+<body>
+    <div class="new-user">
+        <h2> Create new user </h2>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            <label>Username:</label>
+            <input type="text" name="username">
+
+            <label>Email:</label>
+            <input type="text" name="email"> 
+
+            <input type="submit" name="submit" value="submit">       
+        </form>
+    </div>
+</body>
+</html>
